a9b06054faf79e1baa410698e70b0786fd76d3ad
RobertMamur <mamurov0104@gmail.com>
2026-04-01 20:21:35 +0500

    feat: GraphQL nodule integration and first api of it

--- apps/nestar-api/src/app.module.ts
+++ apps/nestar-api/src/app.module.ts
@@ -2,13 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
- 
+ import {GraphQLModule} from '@nestjs/graphql'
+ import { ApolloDriver } from '@nestjs/apollo';
+import { AppResolver } from './app.resolver';
+
 @Module({
   imports: [
 		ConfigModule.forRoot(),
+GraphQLModule.forRoot({
+			driver: ApolloDriver,
+			playground: true,
+			uploads: false,
+			autoSchemaFile: true,
+		
+		}),
   
   ],
-  controllers: [AppController],
-  providers: [AppService],
+	controllers: [AppController], // bu server Rest Api sifatiada run bo'lyapti
+	providers: [AppService, AppResolver], // graphQL sifatida ham run bo'lyapti
 })
 export class AppModule {}
